9cbfdb82342f1c3d9fba5d354e5ecd5229cc1586
dinendashd <86594060+dinendashd@users.noreply.github.com>
2021-06-27 20:34:35 -1200

    Add files via upload

---
+++ Gunscriptsyt/S_O_GunYt.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "Weapon/Gun")]
+public class S_O_GunYt : ScriptableObject
+{
+    public float bulletForce; //THE AMOUNT OF FORCE APPLIED TO THE BULLET//
+    public float fireRate = 0.5F; //TIME BETWEEN BULLETS BEING FIRED//
+    public float reloadTime = 1f; //TIME TO RELOAD WEAPON//
+    public float DelayBeforeFirstShot = 0f; //TIME DELAY BEFORE FIRST SHOT//
+    public float currentAmmo;
+    public float maxAmmo = 30f;
+
+   
+
+    
+    
+}
